154c50e3f86eb4d8c47d297f6afb99aea8d462cd
Josh Morales <jmora047@ucr.edu>
2012-05-14 13:51:22 -0700

    Update SpeedControl

--- SpeedControl.ino
+++ SpeedControl.ino
@@ -4,8 +4,8 @@
 
 /*These are all ints but should be changed if it is safe. We don't know as of yet how large these numbers will get,
 but if we can, we should use a reset mechanism to save space*/
-volatile unsigned long leftSteps = 0;                    //the number of steps recorded from the left wheel
-volatile unsigned long rightSteps = 0;                   //the number of steps recorded from the right wheel
+//volatile unsigned long leftWheelSteps = 0;
+//volatile unsigned long rightWheelSteps = 0;                  //the number of steps recorded from the right wheel
 unsigned int linearDisplacement = 0;                //the linear displacement resulting from the motors. Each time a motor records a step, the linear displacement count increases
 unsigned int angularDisplacement = 0;                        //Each time the left wheel records a step, the counter is incremented, each time the right wheel records a step the counter is decremented
                                                     //wheel records a step, the counter is decremented
@@ -19,21 +19,21 @@ void incrLeftSteps()
 //incrLeftSteps - increments leftWheelSteps. To be used with interrupt
 void incrRightSteps()
 {
-  leftWheelSteps++;
+  rightWheelSteps++;
 }
 
 // getLeftVelocity - returns the left wheel's velocity
 // @return int - the left wheel's velocity
 int getLeftVelocity()
 {
-  return leftSteps / timeDiff; 
+  return leftWheelSteps / timeDiff; 
 }
 
 // getRightVelocity - returns the right wheel's velocity
 // @return int - the right wheel's velocity
 int getRightVelocity()
 {
-  return rightSteps / timeDiff; 
+  return rightWheelSteps / timeDiff; 
 }
 
 // getAngularDisplacement - returns the angular displacement
