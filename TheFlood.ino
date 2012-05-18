e30dcfeadd9a12cf33f67d3b2be603476505f3dd
Shannon <manifestra11@gmail.com>
2012-05-17 19:01:32 -0700

    Update TheFlood.ino

--- TheFlood.ino
+++ TheFlood.ino
@@ -1,32 +1,37 @@
+unsigned char STACK_CAPACITY = 50; //declare outside of the array itself
 void TheFlood()
 {
-   unsigned char STACK_CAPACITY = 50;
    walls[mouseY][mouseX] = getWallConfig(mouseX, mouseY);
    fillSides(sides, mouseX, mouseY);
-   if (checkCell(sides, mouseX, mouseY);
+   if (checkCell(sides, mouseX, mouseY))
    {
-     //faceideal
+     return;
    }
-   else 
+   else
    {
      unsigned char stackSize = 0;
      unsigned char xPosition = mouseX;
      unsigned char yPosition = mouseY;
+     masterPush(sides, xPosition, yPosition);
      do {
        if (stackSize == STACK_CAPACITY)
        {
          //increase capacity
        }
-       mazeMap[yPosition][xPosition] = getSidesMin() + 1;
-       masterPush(sides, xPosition, yPosition);
        uchar cellPos = pop();
        xPosition = getX(cellPos);
        yPosition = getY(cellPos);
        fillSides(sides, xPosition, yPosition);
-       if (!checkCell(sides, xPosition, yPosition)
+       if (!checkCell(sides, xPosition, yPosition))
        {
+          mazeMap[yPosition][xPosition] = ++(getSidesMin(sides));
           masterPush(sides, xPosition, yPosition);
        }
-      while (stackSize > 0);
+       else if (stackSize >= 1)
+       {
+          xPosition = getX(stack[stackSize - 1]);
+          yPosition = getX(stack[stackSize - 1]);
+       }
      }
-         
+     while (stackSize > 0);
+}
